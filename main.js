38f5eb6f8ad8278941e9b6732f811371629c3d95
emmalouisewaugh <emmalouisewaugh@gmail.com>
2024-01-26 22:34:01 +0000

    adding gnome art

---
+++ main.js
@@ -0,0 +1,13 @@
+console .log('           ______     ');
+console .log('        .~`      `\\   ');
+console .log('       /         \\`\\  ')
+console .log('      /           \\ @  ')
+console .log('     |,------------|    ')
+console .log('     |  C ----0---0-\\     _____________')
+console .log('     /   ,-.-   .__),\\   /   ')
+console .log('    /~,         --    | <   ')
+console .log('   /   \\-,           |  \\_____________ ')
+console .log('  |        \\,_,___,_ / \\      ')
+console .log('  ||   |               \\ ')
+console .log('  ||   |         __     |' )
+console .log('  ||   |========{__}====| ')
